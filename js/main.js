28816885725a1bc4371dbdcb44a4e94b79c49f7a
Pavel <serov-pa@yandex.ru>
2015-08-24 16:07:07 +0300

    Готовая навигация

--- js/main.js
+++ js/main.js
@@ -2,13 +2,23 @@ $(document).ready(function(){
 
     $(window).scroll(function() {
         var top = $(document).scrollTop();
-        var top_off = $('.nav').offset();
         if (top < 180){
             $('.nav').removeClass('nav_fixed');
             $('.rast').hide();
+            $('nav ul li').animate({
+                paddingTop:"30px",
+                paddingBottom:"30px"
+
+            },50);
         }else {
             $('.nav').addClass('nav_fixed');
             $('.rast').show();
+            $('nav ul li').animate({
+                paddingTop:"20px",
+                paddingBottom:"20px"
+
+            },50);
+            $('nav ul li a').addClass('topA');
         }
     });
 
