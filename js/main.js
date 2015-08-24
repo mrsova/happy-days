427117ea7f80c9bbea9fea53f868081562d616a4
Pavel <serov-pa@yandex.ru>
2015-08-24 15:06:18 +0300

    15:05 два готовых блока и фиксированное меню

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
+$(document).ready(function(){
 
+    $(window).scroll(function() {
+        var top = $(document).scrollTop();
+        var top_off = $('.nav').offset();
+        if (top < 180){
+            $('.nav').removeClass('nav_fixed');
+            $('.rast').hide();
+        }else {
+            $('.nav').addClass('nav_fixed');
+            $('.rast').show();
+        }
+    });
+
+});
